f838626b7b187b87d820bd071603d67e6df003a1
Alvaro Santos <alvarosstsantos@gmail.com>
2024-11-11 16:18:39 -0300

    :tada: initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from textnode import TextNode, TextType
+
+
+print("hello world")
+
+
+def main():
+    text_node = TextNode("This is a text node",
+                         TextType.BOLD,
+                         "https://www.boot.dev")
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
