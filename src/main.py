e3840deba4abae54f6d6aa813dd09ee091cfc2c2
Alvaro Santos <alvarosstsantos@gmail.com>
2024-11-13 15:14:55 -0300

    :sparkles: generate all html pages tree

--- src/main.py
+++ src/main.py
@@ -16,11 +16,11 @@ def copy_static_content():
 
     copy_tree(static_dir_path, pub_dir_path)
 
-    from_path = path.join(".", "content", "index.md")
+    from_path = path.join(".", "content")
     template_path = path.join(".", "template.html")
-    dest_path = path.join(pub_dir_path, "index.html")
+    dest_path = path.join(pub_dir_path)
 
-    generate_page(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, dest_path)
 
 
 def copy_tree(source: str, destiny: str):
@@ -44,26 +44,35 @@ def extract_title(markdown: str):
     raise ValueError("No H1 Heading found")
 
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {
-          dest_path} using {template_path}")
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for d in listdir(dir_path_content):
+        s = path.join(dir_path_content, d)
+        d = path.join(dest_dir_path, d)
 
-    template_file = open('template.html', 'r')
-    template = template_file.read()
-    template_file.close()
+        if path.isdir(s):
+            makedirs(d)
+            generate_pages_recursive(s, template_path, d)
+        else:
+            d = d[:-2]+"html"
+            print(f"Generating page from {s} to {
+                d} using {template_path}")
 
-    content_file = open(path.join(".", "content", "index.md"), 'r')
-    content_md = content_file.read()
-    content_file.close()
+            template_file = open(template_path, 'r')
+            template = template_file.read()
+            template_file.close()
 
-    content = markdown_to_html_node(content_md).to_html()
-    title = extract_title(content_md)
+            content_file = open(path.join(s), 'r')
+            content_md = content_file.read()
+            content_file.close()
 
-    index_html = template.replace(
-        "{{ Title }}", title).replace("{{ Content }}", content)
+            content = markdown_to_html_node(content_md).to_html()
+            title = extract_title(content_md)
 
-    with open(dest_path, 'w') as f:
-        f.write(index_html)
+            index_html = template.replace(
+                "{{ Title }}", title).replace("{{ Content }}", content)
+
+            with open(d, 'w') as f:
+                f.write(index_html)
 
 
 def main():
